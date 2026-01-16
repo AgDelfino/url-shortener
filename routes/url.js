9392144da794a34b2edad80353504ca1849e9c88
New User <newuser@192.168.1.11>
2026-01-16 12:49:55 -0300

    Finishes the analytics;

--- routes/url.js
+++ routes/url.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {handleGenerateNewUrl, handleDeleteUrl} = require('../controllers/url');
+const {handleGenerateNewUrl, handleDeleteUrl, handleGetAnalytics} = require('../controllers/url');
 
 const router = express.Router();
 
 router.post('/', handleGenerateNewUrl);
-router.delete('/:shortId', handleDeleteUrl);
+router.delete('/:shortId', handleDeleteUrl)
+router.get('/analytics/:shortId', handleGetAnalytics);
 module.exports = router;
